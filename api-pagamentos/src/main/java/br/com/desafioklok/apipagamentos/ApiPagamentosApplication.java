e10ce8bb4c1f6250b747e29c82e7610518b66a0b
Lucas Berti <berti.prog@gmail.com>
2023-06-18 18:29:44 -0300

    criação dos arquivos docker

--- api-pagamentos/src/main/java/br/com/desafioklok/apipagamentos/ApiPagamentosApplication.java
+++ api-pagamentos/src/main/java/br/com/desafioklok/apipagamentos/ApiPagamentosApplication.java
@@ -1,9 +1,13 @@
 package br.com.desafioklok.apipagamentos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableJpaRepositories
 public class ApiPagamentosApplication {
 
 	public static void main(String[] args) {
